17e2a603f42fad728ab189a583b116cac2b5516c
Yan Chernikov <yan@tracnode.com>
2014-09-04 12:21:38 +1000

    Added database info functionality, multiple result searching, and proper connection and disconnection handling for internal methods

--- Refresh/src/Database.cs
+++ Refresh/src/Database.cs
@@ -11,7 +11,8 @@ public class Database {
 
 	private MySqlConnection connection;
 	private DataSet data;
-
+	private List<string> tables = new List<string>();
+	private Dictionary<string, List<string>> columns = new Dictionary<string, List<string>>();
 	private Dictionary<string, MySqlDataAdapter> adapters = new Dictionary<string, MySqlDataAdapter>();
 
 	public Database(string name, string host, string username, string password) {
@@ -20,40 +21,62 @@ public class Database {
 		data = new DataSet();
 	}
 
-	public void Connect() {
+	public bool Connect() {
 		try {
 			if (connection != null) {
-				if (connection.State == ConnectionState.Closed) connection.Open();
-				else Console.WriteLine("Error! Connection already open!");
+				if (connection.State == ConnectionState.Closed) {
+					connection.Open();
+					return true;
+				}
 			}
+			return false;
 		} catch (MySqlException e) {
 			Console.WriteLine(e.Message);
+			return false;
 		}
 	}
 
-	public void Disconnect() {
+	public bool Disconnect() {
 		try {
 			if (connection != null) {
-				if (connection.State == ConnectionState.Open) connection.Close();
-				else Console.WriteLine("Error! Connection already closed!");
+				if (connection.State == ConnectionState.Open) {
+					connection.Close();
+					return true;
+				}
 			}
+			return false;
 		} catch (MySqlException e) {
 			Console.WriteLine(e.Message);
+			return false;
 		}
 	}
 
 	private List<string> GetTableNames() {
-		List<string> result = new List<string>();
+		List<string> results = new List<string>();
+		bool connected = Connect();
 		DataTable data = connection.GetSchema("Tables");
 		foreach (DataRow row in data.Rows) {
-			result.Add(row["TABLE_NAME"].ToString());
+			results.Add(row["TABLE_NAME"].ToString());
 		}
-		return result;
+		if (connected) Disconnect();
+		return results;
+	}
+
+	public List<string> GetColumnNames(string table) {
+		DataTable t = data.Tables[table];
+		if (t == null) return null; // Invalid table name
+		List<string> results = new List<string>();
+		bool connected = Connect();
+		foreach (DataColumn column in t.Columns) {
+			results.Add(column.ColumnName);
+		}
+		if (connected) Disconnect();
+		return results;
 	}
 
 	public void Populate() {
-		Connect();
-		List<string> tables = GetTableNames();
+		bool connected = Connect();
+		tables = GetTableNames();
 		for (int i = 0; i < tables.Count; i++) {
 			adapters.Add(tables[i], CreateAdapter("SELECT * FROM " + tables[i]));
 			try {
@@ -63,7 +86,10 @@ public class Database {
 				continue;
 			}
 		}
-		Disconnect();
+		foreach (string table in tables) {
+			columns.Add(table, GetColumnNames(table));
+		}
+		if (connected) Disconnect();
 	}
 
 	public MySqlCommand CreateCommand(string sql) {
@@ -81,7 +107,7 @@ public class Database {
 		else search[0] = keywords;
 		DataTable t = data.Tables[table];
 		if (t == null) return null; // Invalid table name specified
-		Connect();
+		bool connected = Connect();
 		foreach (DataRow row in t.Rows) {
 			foreach (DataColumn col in t.Columns) {
 				string value = row[col].ToString();
@@ -97,7 +123,7 @@ public class Database {
 				}
 			}
 		}
-		Disconnect();
+		if (connected) Disconnect();
 		return results;
 	}
 
@@ -105,7 +131,7 @@ public class Database {
 		List<string> results = new List<string>();
 		DataTable t = data.Tables[table];
 		if (t == null) return null; // Invalid table name specified
-		Connect();
+		bool connected = Connect();
 		foreach (DataRow row in t.Rows) {
 			foreach (DataColumn col in t.Columns) {
 				string value = row[col].ToString();
@@ -119,14 +145,14 @@ public class Database {
 				}
 			}
 		}
-		Disconnect();
+		if (connected) Disconnect();
 		return results;
 	}
 
 	public DataRow FindRowByKey(string key, string table) {
 		DataTable t = data.Tables[table];
 		if (t == null) return null; // Invalid table name specified
-		Connect();
+		bool connected = Connect();
 		foreach (DataRow row in t.Rows) {
 			foreach (DataColumn col in t.Columns) {
 				string value = row[col].ToString();
@@ -136,8 +162,57 @@ public class Database {
 				}
 			}
 		}
-		Disconnect();
+		if (connected) Disconnect();
 		return null;
+	}
+
+	public DataRow FindRow(string keywords, string table) {
+		DataTable t = data.Tables[table];
+		if (t == null) return null; // Invalid table name specified
+		string[] search = new string[1];
+		if (keywords.Contains(" ")) keywords.Split(keywords.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+		else search[0] = keywords;
+		bool connected = Connect();
+		foreach (DataRow row in t.Rows) {
+			foreach (DataColumn col in t.Columns) {
+				string value = row[col].ToString();
+				foreach (string s in search) {
+					if (value.Contains(s)) {
+						Disconnect();
+						return row;
+					}
+				}
+			}
+		}
+		if (connected) Disconnect();
+		return null;
+	}
+
+	public List<DataRow> FindRows(string keywords, string table) {
+		List<DataRow> results = new List<DataRow>();
+		DataTable t = data.Tables[table];
+		if (t == null) return null; // Invalid table name specified
+		string[] search = new string[1];
+		if (keywords.Contains(" ")) keywords.Split(keywords.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+		else search[0] = keywords;
+		bool connected = Connect();
+		foreach (DataRow row in t.Rows) {
+			foreach (DataColumn col in t.Columns) {
+				string value = row[col].ToString();
+				foreach (string s in search) {
+					if (value.Contains(s)) {
+						results.Add(row);
+						break;
+					}
+				}
+			}
+		}
+		if (connected) Disconnect();
+		return results;
+	}
+
+	public bool Update(string key, string table) {
+		return false;
 	}
 
 	public bool Delete(string key, string table) {
@@ -147,6 +222,18 @@ public class Database {
 		adapters[table].DeleteCommand = new MySqlCommandBuilder(adapters[table]).GetDeleteCommand();
 		adapters[table].Update(data, table);
 		return true;
+	}
+
+	public List<string> Tables {
+		get { return tables; }
+	}
+
+	public List<string> GetColumns(string table) {
+		return columns[table];
+	}
+
+	public Dictionary<string, List<string>> AllColumns {
+		get { return columns; }
 	}
 
 }
--- Refresh/src/TestProgram.cs
+++ Refresh/src/TestProgram.cs
@@ -1,18 +1,60 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
+
 
 public class TestProgram {
 
-	public static void Main(string[] args) {
-		Database database = new Database("archery_db", "localhost", "root", "021190");
-		database.Populate();
+	private Database database;
 
+	private TestProgram() {
+		database = new Database("archery_db", "localhost", "root", "021190");
+		database.Populate();
+	}
+
+	private void GetInfo() {
+		Console.WriteLine("Tables");
+		Console.WriteLine("======");
+		foreach (string table in database.Tables) {
+			Console.WriteLine(table);
+		}
+		Console.WriteLine("\n");
+
+		Console.WriteLine("Columns");
+		Console.WriteLine("=======");
+		foreach (string s in database.GetColumns("archer")) {
+			Console.WriteLine(s);
+		}
+		//foreach (string col in database.AllColumns["archer"]) {
+		//		Console.WriteLine(col);
+		//	}
+	}
+
+	private void Search() {
 		List<string> results = database.FindExact("M", "archer");
 		if (results.Count == 0) Console.WriteLine("No results found.");
 		foreach (string s in results) {
 			Console.WriteLine(s);
 		}
+	}
 
+	private void Rows() {
+		List<DataRow> rows = database.FindRows("FITA", "face");
+		foreach (DataRow row in rows) {
+			for (int i = 0; i < row.ItemArray.Length; i++) {
+				Console.Write(row[i] + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+
+	private void Delete() {
 		database.Delete("120", "archer");
+	}
+
+
+	public static void Main(string[] args) {
+		TestProgram test = new TestProgram();
+		test.GetInfo();
 	}
 }
